feb67edc0d28319e767b8d60a9f685318398f184
Marek Pankowski <17934750+mkpanq@users.noreply.github.com>
2024-10-17 11:44:20 +0200

    todo comment

--- app/api_clients/ip_api_client.rb
+++ app/api_clients/ip_api_client.rb
@@ -2,6 +2,7 @@ class IpApiClient < ApiClient
   API_BASE_URL = "http://ip-api.com/json/"
   API_FIELDS_PARAMS_CODE="1079295"
 
+  # TODO: Could create factory, but rescue errors, are too much depend on Faraday implementaion
   def initialize(http_client = FaradayHttpClient.new(API_BASE_URL, params = {
     "fields" => API_FIELDS_PARAMS_CODE
   }))
