99776c4932cfbd25de966ecb703222bab51b8cfa
Marek Pankowski <17934750+mkpanq@users.noreply.github.com>
2024-10-17 10:45:44 +0200

    Fixing repository name, refactor services

---
+++ app/services/data_service_factory.rb
@@ -0,0 +1,5 @@
+class DataServiceFactory
+  def self.create_ip_location_data_service
+    IpLocationDataService.new
+  end
+end
--- app/services/ip_location_data_service.rb
+++ app/services/ip_location_data_service.rb
@@ -1,10 +1,14 @@
-# TODO: Refactor
-class LocationDataService
-  def initialize
-    @repository = GeoLocationRepository.new
-    @data_provider = IpApiClient.new
-    @query_validator = IpApiQueryValidator.new
-    @data_serializer = IpApiDataSerializer.new
+class IpLocationDataService < DataService
+  def initialize(
+    repository = GeoLocationRepository.new,
+    data_provider = IpApiClient.new,
+    query_validator = IpApiQueryValidator.new,
+    data_serializer = IpApiDataSerializer.new
+  )
+    @repository = repository
+    @data_provider = data_provider
+    @query_validator = query_validator
+    @data_serializer = data_serializer
   end
 
   def get_geological_data(query)
@@ -13,19 +17,21 @@ class LocationDataService
   end
 
   def add_geological_data(query, refresh = false)
+    @query_validator.valid?(query)
     potential_location = @repository.find_location_by_query(query)
-
     if potential_location
       raise CustomErrors::DataAlreadyExistsError unless refresh
 
       data = @data_provider.call(query)
-      @repository.update(potential_location, data)
+      serialized_data = @data_serializer.serialize(query, data)
+      @repository.update(potential_location, serialized_data)
 
       return potential_location
     end
 
     data = @data_provider.call(query)
-    @repository.create(data)
+    serialized_data = @data_serializer.serialize(query, data)
+    @repository.create(serialized_data)
   end
 
   def delete_geological_data(query)
